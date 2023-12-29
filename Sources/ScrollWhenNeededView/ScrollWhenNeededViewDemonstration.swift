a207a2974f49f486a626901bf5b09d1c2adbea5e
Michael Colonna <mcolonnajr@gmail.com>
2023-12-29 11:30:51 +0100

    Commit initial code base

---
+++ Sources/ScrollWhenNeededView/ScrollWhenNeededViewDemonstration.swift
@@ -0,0 +1,57 @@
+//
+//  ScrollWhenNeededViewDemonstration.swift
+//
+//
+//  Created by Michael Colonna on 12/27/23.
+//
+
+import SwiftUI
+
+struct ScrollWhenNeededViewDemonstration: View {
+    
+    // MARK: - Properties
+    // MARK: Private Properties
+    @State private var itemCount: Int = 10
+
+    // MARK: - Views
+    // MARK: Body
+    var body: some View {
+        NavigationStack {
+            ScrollWhenNeededView {
+                VStack {
+                    #if os(watchOS)
+                    HStack {
+                        Button(action: { itemCount = max(itemCount - 1, 0) }) {
+                            Text("-")
+                        }
+                        .padding()
+                        
+                        Button(action: { itemCount += 1 }) {
+                            Text("+")
+                        }
+                        .padding()
+                    }
+                    #else
+                    Stepper("Number of Items: \(itemCount)", value: $itemCount, in: 0...20)
+                        .padding()
+                    #endif
+                    
+                    ForEach(0..<itemCount, id: \.self) { index in
+                        Text("Item \(index)").padding()
+                            .frame(maxWidth: .infinity)
+                    }
+                    .frame(maxWidth: .infinity)
+                }
+            }
+            .navigationTitle("ScrollWhenNeededView")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+// MARK: - Preview
+struct ScrollWhenNeededViewDemonstration_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrollWhenNeededViewDemonstration()
+    }
+}
